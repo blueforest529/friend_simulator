859200c6c47587c95817d41d96ac8cb8bab74e52
Im Cheong <imcheong001@e-mirim.hs.kr>
2017-04-06 20:54:14 +0900

    #10 로그남기기

--- app/src/main/java/kr/hs/e_mirim/cheong/yebonlee/MainActivity.java
+++ app/src/main/java/kr/hs/e_mirim/cheong/yebonlee/MainActivity.java
@@ -2,35 +2,48 @@ package kr.hs.e_mirim.cheong.yebonlee;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public final static String TAG = "이예본 : Main";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "메소드 호출");
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "레이아웃 세팅");
 
     }
     void onClick(View view){
+        Log.d(TAG, "메소드 호출");
+        Log.d(TAG, "클릭된 view id"+view.getId());
         switch (view.getId()){
             case  R.id.button1 :
+                Log.d(TAG, "1 호출");
                 Toast.makeText(this,"이이~!~",Toast.LENGTH_SHORT).show();
                 break;
             case  R.id.button2 :
+                Log.d(TAG, "2 호출");
                 Toast.makeText(this,"방구마려워",Toast.LENGTH_SHORT).show();
                 break;
             case  R.id.button3 :
+                Log.d(TAG, "3 호출");
                 Toast.makeText(this,"집가고 싶다",Toast.LENGTH_SHORT).show();
                 break;
             case  R.id.button4 :
+                Log.d(TAG, "4 호출");
                 Toast.makeText(this,"청아 뭐해?",Toast.LENGTH_SHORT).show();
                 break;
             case  R.id.button5 :
+                Log.d(TAG, "5 호출");
                 Toast.makeText(this,"예보니아",Toast.LENGTH_SHORT).show();
                 break;
-           default: Toast.makeText(this,"뭐라는 지 모르겠음~",Toast.LENGTH_SHORT).show();
+           default:
+               Log.d(TAG, "모르는 녀석 클릭");
+               Toast.makeText(this,"뭐라는 지 모르겠음~",Toast.LENGTH_SHORT).show();
 
         }
 
